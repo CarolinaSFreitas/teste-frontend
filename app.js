25cc231425e88bf19a9c645eadfccbbe829886f6
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2025-07-02 15:30:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = process.env.MONGOPORT|| 3333;
+const port = process.env.MONGOPORT|| 27017;
 
 const app = express();
 
